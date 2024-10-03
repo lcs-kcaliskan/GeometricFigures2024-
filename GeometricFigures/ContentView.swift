dae8b9bb7cd2adf077777876bdd4e339ceaee6a1
Kaan Caliskan <kcaliskan@lcs.on.ca>
2024-10-03 08:55:08 -0400

    Initial Commit

---
+++ GeometricFigures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeometricFigures
+//
+//  Created by Ahmet Kaan Caliskan on 2024-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
